8e48346cc5a631588fca3edf2669988edf99f2fa
Utkarsh Jhariya <87616610+utquarsh027@users.noreply.github.com>
2023-12-24 16:17:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 import google.ai.generativelanguage as glm
 from PIL import Image
 
-API_KEY="AIzaSyAo9yfpvJACfzgxPyX3cj3FkSoV4wUy3nY"
+API_KEY=st.secrets['key']
 genai.configure(api_key=API_KEY)
 
 st.set_page_config(page_title="Generate story", 
